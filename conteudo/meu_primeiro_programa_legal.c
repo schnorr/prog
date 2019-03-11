31a89a72af5cfc01ec6714afa56acfc0bb8dc6f7
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2019-03-11 15:23:15 -0300

    primeiro programa, exemplo

---
+++ conteudo/meu_primeiro_programa_legal.c
@@ -0,0 +1,15 @@
+/*
+  Este programa pega dois inteiros, os soma e exibe o
+  resultado na tela
+*/
+#include <stdio.h>
+int main(void)
+{
+  int a, b; //Declaração da variável com os parâmetros
+  int soma; //Declaração da variável com o resultado
+  a = 28929872 + 9;
+  b = 38765876;
+  soma = a + b;
+  printf("Resultado da soma: %d\n", soma);
+  return 0;
+}
