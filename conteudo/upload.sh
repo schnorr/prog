22b74ff9d775926c0a13f3b0f8b8deec4fe111ba
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2019-03-27 12:05:06 -0300

    Makefile and upload.sh

---
+++ conteudo/upload.sh
@@ -0,0 +1,16 @@
+PDFS="apresentacao.pdf \
+introducao.pdf \
+revisao-aula-01.pdf \
+sequencial.pdf \
+revisao-aula-02.pdf \
+tipos.pdf \
+revisao-aula-03.pdf \
+if.pdf \
+revisao-aula-04.pdf \
+if-else.pdf \
+revisao-aula-05.pdf \
+switch.pdf"
+
+COMMAND="rsync -rv --progress $PDFS html.inf.ufrgs.br:./public_html/inf1202/"
+echo $COMMAND
+eval $COMMAND
