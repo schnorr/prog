f02bbff2c27140b5a3ef293dcfe7cc0b65e07e54
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2019-04-22 17:54:40 -0300

    update part that has been coded together

--- conteudo/a17-notas.c
+++ conteudo/a17-notas.c
@@ -4,4 +4,25 @@
 int main(){
   //10 alunos, cada um com 4 notas
   float notas[NALUNOS][NNOTAS];
+  int a, n, soma, contador = 0;
+  float media;
+  for (a = 0; a < NALUNOS; a++){
+    for (n = 0; n < NNOTAS; n++){
+      scanf("%f", &notas[a][n]);
+    }
+  }
+
+  soma = 0;
+  for (a = 0; a < NALUNOS; a++){
+    soma += notas[a][0];
+  }
+  media = (float)soma/NALUNOS;
+  //printf da media
+
+  contador = 0;
+  for (a = 0; a < NALUNOS; a++){
+    if (notas[a][0] > media) contador++;
+  }
+  //printf do contador
+  return 0;
 }
