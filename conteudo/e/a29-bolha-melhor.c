889e57b08b61c197e7799879371abf7d3d2b663a
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2019-05-22 15:17:27 -0300

    fixes during class

--- conteudo/e/a29-bolha-melhor.c
+++ conteudo/e/a29-bolha-melhor.c
@@ -8,9 +8,9 @@
 #define TAM 6
 void ordena_pela_bolha (int *v, int tamanho) {
   int i, sinal, aux, fim;
+  fim = tamanho - 1;
   do {
     sinal = 0;
-    fim = tamanho - 1;
     for (i = 0; i < fim; i++)
       if (v[i] > v[i+1]) {
 	aux = v[i];
@@ -20,12 +20,4 @@ void ordena_pela_bolha (int *v, int tamanho) {
       }
     fim--;
   } while (sinal == 1);
-}
-
-int main() {
-int vetor[TAM] = {5, 3, 4, 1, 9, 8};
-ordena_pela_bolha(vetor, TAM);
-for(int i = 0; i < TAM; i++)
-  printf("%d ", *(vetor+i));
-return 0;
 }
