84087d24167c7078771d7232b654a67ed7aacb15
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2022-07-10 23:04:20 -0300

    update slides

--- conteudo/e/rev-a08-taxa.c
+++ conteudo/e/rev-a08-taxa.c
@@ -5,10 +5,10 @@ int main() {
   double popBRA, popEUA;
   double taxaBRA, taxaEUA;
 
-  popBRA = 203.0;
-  popEUA = 318.0;
-  taxaBRA = 1.5 / 100;
-  taxaEUA = 1.3 / 100;
+  popBRA = 214.0;
+  popEUA = 332.0;
+  taxaBRA = 1.34 / 100;
+  taxaEUA = 1.09 / 100;
 
   for (ano = 0; popBRA < popEUA; ano++){
     popBRA += popBRA * taxaBRA;
