9cdd244a6774fee6cd674937c5808a9410949d8c
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2022-08-05 23:23:14 -0300

    save

--- conteudo/e/aula13-bubble-strings.c
+++ conteudo/e/aula13-bubble-strings.c
@@ -4,24 +4,34 @@
 #define TAMANHO 10
 #define NLINHAS 4
 int main() {
-  char nomes[NLINHAS][TAMANHO] = { "Lucas", "Theo", "Alba", "Elisa" };
+  char n[NLINHAS][TAMANHO] =
+    { "Lucas",
+      "Theo",
+      "Alba",
+      "Elisa" };
   char aux[TAMANHO] = {0};
-  int sinal; // sinal de troca do algoritmo
-  int linha; // índica da linha
+  int sinal; // sinal de troca
+  int i; // índica da linha
+
+
+
+
   // Ordenação por Bolha
   do {
     sinal = 0;
-    for (linha = 0; linha < NLINHAS-1; linha++)
-      if (strcmp(nomes[linha], nomes[linha+1]) > 0) {
-	  strncpy(aux,            nomes[linha],   TAMANHO);
-	  strncpy(nomes[linha],   nomes[linha+1], TAMANHO);
-	  strncpy(nomes[linha+1], aux,            TAMANHO);
+    for (i = 0; i < NLINHAS-1; i++)
+      if (strcmp(n[i], n[i+1]) > 0) {
+	  strcpy(aux, n[i]);
+	  strcpy(n[i], n[i+1]);
+	  strcpy(n[i+1], aux);
 	  sinal = 1;
       }
   } while (sinal == 1);
+
+
   // Imprimir o resultado do ordenamento
-  for (linha = 0; linha < NLINHAS; linha++){
-    printf("%d [%s]\n", linha, nomes[linha]);
+  for (i = 0; i < NLINHAS; i++){
+    printf("%d [%s]\n", i, n[i]);
   }
   return 0;
 }
