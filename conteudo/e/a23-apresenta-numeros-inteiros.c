3a7823cf9d2e704c5d54d2f65cf87a00324d48ba
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2022-08-19 18:23:46 -0300

    save

--- conteudo/e/a23-apresenta-numeros-inteiros.c
+++ conteudo/e/a23-apresenta-numeros-inteiros.c
@@ -15,3 +15,21 @@ int main() {
   printf("\n");
   return 0;
 }
+
+#include <stdio.h>
+int main() {
+  int i;
+  for (i=1;i<20;i++)
+    printf("*");
+  printf("\n");
+  printf("Numeros entre 1 e 5\n");
+  for (i=1;i<20;i++)
+    printf("*");
+  printf("\n");
+  for (i=1;i<=5;i++)
+    printf("%d\n",i);
+  for (i=1;i<20;i++)
+    printf("*");
+  printf("\n");
+  return 0;
+}
--- conteudo/e/a23-motivacao-funcao.c
+++ conteudo/e/a23-motivacao-funcao.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #define MAX 10
-int a[MAX]; //global
-int imprime_vetor () {
+int imprime_vetor (int v[]) {
   int k;
   for (k=0; k<MAX; k++)
-    printf("%3d ",a[k]);
+    printf("%3d ",v[k]);
   printf("\n");
   return 0;
 }
 int main() {
-  int k;
+  int k, a[MAX];
   //preencher com índices
   for (k=0; k<MAX; k++)
     a [k] = k;
   //imprimir a
-  imprime_vetor();
+  imprime_vetor(a);
   //somar 2 em cada
   for (k=0; k<MAX; k++)
     a [k] = a[k] + 2;
   //imprimir a
-  imprime_vetor();
+  imprime_vetor(a);
   //zerar os pares
   for (k=0; k<MAX; k++)
     if (a[k] % 2 == 0)
       a[k] = 0;
   //imprimir a
-  imprime_vetor();
+  imprime_vetor(a);
   return 0;
 }
