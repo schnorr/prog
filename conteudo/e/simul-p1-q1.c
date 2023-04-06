c90b609474ff691ef5695907a3d1e45efec42ebf
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2023-04-06 16:09:20 -0300

    indent

--- conteudo/e/simul-p1-q1.c
+++ conteudo/e/simul-p1-q1.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-int main ()
+int main()
 {
-int x, i, j;
-x = 8;
-for (i = 2; i < 5; i++)
-{
-j= 3;
-while (j > 0)
-{
-x = x+1;
-j = j-1;
-}
-printf( "X : %5d\n", x );
-}
-printf("- Fim - \n");
-return 0;
+    int x, i, j;
+    x = 8;
+    for (i = 2; i < 5; i++) {
+	j = 3;
+	while (j > 0) {
+	    x = x + 1;
+	    j = j - 1;
+	}
+	printf("X : %5d\n", x);
+    }
+    printf("- Fim - \n");
+    return 0;
 }
