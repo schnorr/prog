f9c3e66d59f54d11b60d540a087d1b3020ffe168
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2024-03-20 15:12:12 -0300

    lowercase

--- conteudo/e/programa-conversao-real-dolar.c
+++ conteudo/e/programa-conversao-real-dolar.c
@@ -2,13 +2,13 @@
 #include <stdio.h> // biblioteca com operações de I/O
 int main()
 {
-  float Preco, Taxa;          // Valores em real declarados float
-  float PrecoEmDolar;         // tipo da variável resultante
+  float preco, taxa;          // Valores em real declarados float
+  float precoemdolar;         // tipo da variável resultante
   printf("Valor em real:\n"); // mensagem impressa na tela
-  scanf("%f", &Preco);        // leitura da informação digitada
+  scanf("%f", &preco);        // leitura da informação digitada
   printf("Taxa conversao :\n");
-  scanf("%f", &Taxa);
-  PrecoEmDolar = Preco *Taxa;
-  printf("Preco em dolar: %f\n", PrecoEmDolar); // mostra
+  scanf("%f", &taxa);
+  precoemdolar = preco / taxa;
+  printf("Preco em dolar: %.2f\n", precoemdolar); // mostra
   return 0; // encerra o programa e retorna zero ao Sist Op
 }
